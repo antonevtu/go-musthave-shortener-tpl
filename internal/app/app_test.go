dcc7517271cc55530aaacd26016962d2df2556d7
antonevtu <antonevtu@mail.ru>
2021-12-29 16:38:00 +0300

    Инкремент 8

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -47,114 +47,114 @@ func TestGZipJSONAPI(t *testing.T) {
 	require.NoError(t, err)
 }
 
-//func TestJSONAPI(t *testing.T) {
-//	cfgApp := config(t)
-//	//_ = os.Remove(cfgApp.FileStoragePath)
-//
-//	repo, err := repository.New(cfgApp.FileStoragePath)
-//	assert.Equal(t, err, nil)
-//
-//	r := handlers.NewRouter(repo, cfgApp)
-//	ts := httptest.NewServer(r)
-//	defer ts.Close()
-//
-//	// Create ID1
-//	longURL := "https://yandex.ru/maps/geo/sochi/53166566/?ll=39.580041%2C43.713351&z=9.98"
-//	buf := testEncodeJSONLongURL(longURL)
-//	resp, shortURLInJSON := testRequest(t, ts.URL+"/api/shorten", "POST", buf)
-//	err = resp.Body.Close()
-//	require.NoError(t, err)
-//	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-//
-//	// Parse shortURL
-//	shortURL := testDecodeJSONShortURL(t, shortURLInJSON)
-//	_, err = url.Parse(shortURL)
-//	require.NoError(t, err)
-//
-//	// Create ID2
-//	longURL = "https://habr.com/ru/all/"
-//	buf = testEncodeJSONLongURL(longURL)
-//	resp, shortURLInJSON = testRequest(t, ts.URL+"/api/shorten", "POST", buf)
-//	err = resp.Body.Close()
-//	require.NoError(t, err)
-//	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-//
-//	// Parse shortURL
-//	shortURL = testDecodeJSONShortURL(t, shortURLInJSON)
-//	u, err := url.Parse(shortURL)
-//	require.NoError(t, err)
-//
-//	// Check redirection by existing ID
-//	resp, _ = testRequest(t, ts.URL+u.Path, "GET", nil)
-//	err = resp.Body.Close()
-//	require.NoError(t, err)
-//	assert.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode)
-//	longURLRecovered := resp.Header.Get("Location")
-//	assert.Equal(t, longURL, longURLRecovered)
-//
-//	// Check StatusBadRequest for incorrect JSON key in request
-//	badJSON := `{"urlBad":"abc"}`
-//	resp, _ = testRequest(t, ts.URL+"/api/shorten", "POST", bytes.NewBufferString(badJSON))
-//	err = resp.Body.Close()
-//	require.NoError(t, err)
-//	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-//
-//	// Check server returns correct baseURL
-//	baseURL := u.Scheme + "://" + u.Host
-//	assert.Equal(t, baseURL, cfgApp.BaseURL)
-//
-//	// Check storage file created
-//	file, err := os.Open(cfgApp.FileStoragePath)
-//	assert.Equal(t, err, nil)
-//	defer file.Close()
-//}
+func TestJSONAPI(t *testing.T) {
+	cfgApp := config(t)
+	//_ = os.Remove(cfgApp.FileStoragePath)
 
-//func TestTextAPI(t *testing.T) {
-//	var cfgApp = config(t)
-//	//_ = os.Remove(cfgApp.FileStoragePath)
-//	repo, err := repository.New(cfgApp.FileStoragePath)
-//	assert.Equal(t, err, nil)
-//	r := handlers.NewRouter(repo, cfgApp)
-//	ts := httptest.NewServer(r)
-//	defer ts.Close()
-//
-//	// Create ID
-//	longURL := "https://yandex.ru/maps/geo/sochi/53166566/?ll=39.580041%2C43.713351&z=9.98"
-//	resp, shortURL_ := testRequest(t, ts.URL, "POST", bytes.NewBufferString(longURL))
-//	err = resp.Body.Close()
-//	require.NoError(t, err)
-//	assert.Equal(t, http.StatusCreated, resp.StatusCode)
-//
-//	// Parse shortURL
-//	u, err := url.Parse(shortURL_)
-//	require.NoError(t, err)
-//
-//	// Check redirection by existing ID
-//	resp, _ = testRequest(t, ts.URL+u.Path, "GET", nil)
-//	err = resp.Body.Close()
-//	require.NoError(t, err)
-//	assert.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode)
-//	longURLRecovered := resp.Header.Get("Location")
-//	assert.Equal(t, longURL, longURLRecovered)
-//
-//	// Check StatusBadRequest for non existed ID
-//	shortURL1 := ts.URL + u.Path + "xxx"
-//	resp, _ = testRequest(t, shortURL1, "GET", nil)
-//	err = resp.Body.Close()
-//	require.NoError(t, err)
-//	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-//
-//	// Check not allowed method error
-//	resp, _ = testRequest(t, ts.URL+u.Path, "PUT", nil)
-//	err = resp.Body.Close()
-//	require.NoError(t, err)
-//	assert.Equal(t, http.StatusMethodNotAllowed, resp.StatusCode)
-//
-//	// Check storage file created
-//	file, err := os.Open(cfgApp.FileStoragePath)
-//	assert.Equal(t, err, nil)
-//	defer file.Close()
-//}
+	repo, err := repository.New(cfgApp.FileStoragePath)
+	assert.Equal(t, err, nil)
+
+	r := handlers.NewRouter(repo, cfgApp)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	// Create ID1
+	longURL := "https://yandex.ru/maps/geo/sochi/53166566/?ll=39.580041%2C43.713351&z=9.98"
+	buf := testEncodeJSONLongURL(longURL)
+	resp, shortURLInJSON := testRequest(t, ts.URL+"/api/shorten", "POST", buf)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	// Parse shortURL
+	shortURL := testDecodeJSONShortURL(t, shortURLInJSON)
+	_, err = url.Parse(shortURL)
+	require.NoError(t, err)
+
+	// Create ID2
+	longURL = "https://habr.com/ru/all/"
+	buf = testEncodeJSONLongURL(longURL)
+	resp, shortURLInJSON = testRequest(t, ts.URL+"/api/shorten", "POST", buf)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	// Parse shortURL
+	shortURL = testDecodeJSONShortURL(t, shortURLInJSON)
+	u, err := url.Parse(shortURL)
+	require.NoError(t, err)
+
+	// Check redirection by existing ID
+	resp, _ = testRequest(t, ts.URL+u.Path, "GET", nil)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode)
+	longURLRecovered := resp.Header.Get("Location")
+	assert.Equal(t, longURL, longURLRecovered)
+
+	// Check StatusBadRequest for incorrect JSON key in request
+	badJSON := `{"urlBad":"abc"}`
+	resp, _ = testRequest(t, ts.URL+"/api/shorten", "POST", bytes.NewBufferString(badJSON))
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+
+	// Check server returns correct baseURL
+	baseURL := u.Scheme + "://" + u.Host
+	assert.Equal(t, baseURL, cfgApp.BaseURL)
+
+	// Check storage file created
+	file, err := os.Open(cfgApp.FileStoragePath)
+	assert.Equal(t, err, nil)
+	defer file.Close()
+}
+
+func TestTextAPI(t *testing.T) {
+	var cfgApp = config(t)
+	//_ = os.Remove(cfgApp.FileStoragePath)
+	repo, err := repository.New(cfgApp.FileStoragePath)
+	assert.Equal(t, err, nil)
+	r := handlers.NewRouter(repo, cfgApp)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	// Create ID
+	longURL := "https://yandex.ru/maps/geo/sochi/53166566/?ll=39.580041%2C43.713351&z=9.98"
+	resp, shortURL_ := testRequest(t, ts.URL, "POST", bytes.NewBufferString(longURL))
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	// Parse shortURL
+	u, err := url.Parse(shortURL_)
+	require.NoError(t, err)
+
+	// Check redirection by existing ID
+	resp, _ = testRequest(t, ts.URL+u.Path, "GET", nil)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode)
+	longURLRecovered := resp.Header.Get("Location")
+	assert.Equal(t, longURL, longURLRecovered)
+
+	// Check StatusBadRequest for non existed ID
+	shortURL1 := ts.URL + u.Path + "xxx"
+	resp, _ = testRequest(t, shortURL1, "GET", nil)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+
+	// Check not allowed method error
+	resp, _ = testRequest(t, ts.URL+u.Path, "PUT", nil)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusMethodNotAllowed, resp.StatusCode)
+
+	// Check storage file created
+	file, err := os.Open(cfgApp.FileStoragePath)
+	assert.Equal(t, err, nil)
+	defer file.Close()
+}
 
 func testGZipRequest(t *testing.T, url, method string, body io.Reader) (*http.Response, string) {
 	client := &http.Client{}
@@ -166,9 +166,11 @@ func testGZipRequest(t *testing.T, url, method string, body io.Reader) (*http.Re
 	gz := gzip.NewWriter(&buf)
 	defer gz.Close()
 	body_, err := io.ReadAll(body)
-	n, err := gz.Write(body_)
-	_ = n
+	require.NoError(t, err)
+	_, err = gz.Write(body_)
+	require.NoError(t, err)
 	err = gz.Flush()
+	require.NoError(t, err)
 
 	req, err := http.NewRequest(method, url, &buf)
 	req.Header.Set("Content-Encoding", "gzip")
